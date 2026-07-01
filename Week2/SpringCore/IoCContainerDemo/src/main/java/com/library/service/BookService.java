628f158f5b8a8e80f99680d437db6e25b274282c
AsMetOP <176515010+AsMetOP@users.noreply.github.com>
2026-07-01 14:18:57 +0530

    Week2: Ex5 - Spring IoC Container Configuration complete

--- Week2/SpringCore/IoCContainerDemo/src/main/java/com/library/service/BookService.java
+++ Week2/SpringCore/IoCContainerDemo/src/main/java/com/library/service/BookService.java
@@ -1,13 +1,21 @@
 package com.library.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.library.repository.BookRepository;
 
+@Service
 public class BookService {
 
     private BookRepository bookRepository;
 
+    @Autowired
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    public String getBookDetails(int id) {
+        return "Fetching details for book id " + id;
     }
 
     public String getBookFromRepository(int id) {
