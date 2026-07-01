9cc00e91a83159e6704fd61a4f5e1c84c8a69265
AsMetOP <176515010+AsMetOP@users.noreply.github.com>
2026-07-01 13:23:12 +0530

    Week2: Ex2 - Dependency Injection complete

--- Week2/SpringCore/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ Week2/SpringCore/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -1,8 +1,20 @@
 package com.library.service;
 
+import com.library.repository.BookRepository;
+
 public class BookService {
+
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     public String getBookDetails(int id) {
         return "Fetching details for book id " + id;
+    }
+
+    public String getBookFromRepository(int id) {
+        return bookRepository.findBookById(id);
     }
 }
