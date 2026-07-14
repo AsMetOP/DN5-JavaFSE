85dd583bfba4fb25cdc975ac99f8ad5de7df5684
AsMetOP <176515010+AsMetOP@users.noreply.github.com>
2026-07-15 00:27:37 +0530

    Week3: Ex10 - GetCountryByCode complete

--- Week3/SpringREST/2_spring_rest_handson/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++ Week3/SpringREST/2_spring_rest_handson/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -4,18 +4,24 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognizant.springlearn.Country;
+import com.cognizant.springlearn.service.CountryService;
 
 @RestController
 public class CountryController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+
+	@Autowired
+	private CountryService countryService;
 
 	@RequestMapping("/country")
 	public Country getCountryIndia() {
@@ -34,6 +40,14 @@ public class CountryController {
 		List<Country> countryList = (List<Country>) context.getBean("countryList");
 		LOGGER.info("END");
 		return countryList;
+	}
+
+	@GetMapping("/countries/{code}")
+	public Country getCountry(@PathVariable String code) {
+		LOGGER.info("START");
+		Country result = countryService.getCountry(code);
+		LOGGER.info("END");
+		return result;
 	}
 
 }
