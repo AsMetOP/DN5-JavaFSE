98a1fadffbc147b99d59d4829c9d7432ce5624c6
AsMetOP <176515010+AsMetOP@users.noreply.github.com>
2026-07-14 13:57:08 +0530

    Hibernate: Ex2 - Employee and Department entities complete

---
+++ Week2/Hibernate/EmployeeManagementSystem/src/main/java/com/cognizant/employeemanagementsystem/model/Department.java
@@ -0,0 +1,29 @@
+package com.cognizant.employeemanagementsystem.model;
+
+import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "department")
+@Getter
+@Setter
+@ToString(exclude = "employeeList")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employeeList;
+}
