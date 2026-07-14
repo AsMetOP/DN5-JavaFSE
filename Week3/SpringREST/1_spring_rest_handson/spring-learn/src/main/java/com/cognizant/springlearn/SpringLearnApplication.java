a531c3f220b86d1cc9683260f74a3f19f222c73c
AsMetOP <176515010+AsMetOP@users.noreply.github.com>
2026-07-14 23:12:55 +0530

    Week3: Ex2 - LoadDateFormatFromXML complete

--- Week3/SpringREST/1_spring_rest_handson/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ Week3/SpringREST/1_spring_rest_handson/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -1,9 +1,14 @@
 package com.cognizant.springlearn;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
@@ -13,7 +18,17 @@ public class SpringLearnApplication {
 	public static void main(String[] args) {
 		LOGGER.info("START main");
 		SpringApplication.run(SpringLearnApplication.class, args);
+		displayDate();
 		LOGGER.info("END main");
+	}
+
+	public static void displayDate() {
+		LOGGER.info("START");
+		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		SimpleDateFormat dateFormat = context.getBean("dateFormat", SimpleDateFormat.class);
+		String date = dateFormat.format(new Date());
+		LOGGER.debug(date);
+		LOGGER.info("END");
 	}
 
 }
