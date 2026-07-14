e23872846f03593e5de61b912bd448dabc337299
AsMetOP <176515010+AsMetOP@users.noreply.github.com>
2026-07-14 23:27:01 +0530

    Week3: Ex5 - SingletonPrototypeScope complete

--- Week3/SpringREST/1_spring_rest_handson/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ Week3/SpringREST/1_spring_rest_handson/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -36,6 +36,7 @@ public class SpringLearnApplication {
 		LOGGER.info("START");
 		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
 		Country country = context.getBean("country", Country.class);
+		Country anotherCountry = context.getBean("country", Country.class);
 		LOGGER.debug("Country : {}", country.toString());
 		LOGGER.info("END");
 	}
