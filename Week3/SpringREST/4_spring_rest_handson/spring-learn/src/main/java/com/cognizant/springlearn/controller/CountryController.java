29d91f4a8ccf9b95a28fea1e12bb496ca0664552
AsMetOP <176515010+AsMetOP@users.noreply.github.com>
2026-07-15 05:11:58 +0530

    Week3: Ex18 - ValidAnnotationGlobalHandler complete

--- Week3/SpringREST/4_spring_rest_handson/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++ Week3/SpringREST/4_spring_rest_handson/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -1,16 +1,6 @@
 package com.cognizant.springlearn.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,13 +10,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.cognizant.springlearn.Country;
 import com.cognizant.springlearn.service.CountryService;
 import com.cognizant.springlearn.service.exception.CountryNotFoundException;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/countries")
@@ -56,24 +48,9 @@ public class CountryController {
 	}
 
 	@PostMapping
-	public Country addCountry(@RequestBody Country country) {
+	public Country addCountry(@RequestBody @Valid Country country) {
 		LOGGER.info("Start");
 		LOGGER.info("Country : {}", country.toString());
-
-		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-		Validator validator = factory.getValidator();
-
-		Set<ConstraintViolation<Country>> violations = validator.validate(country);
-		List<String> errors = new ArrayList<String>();
-
-		for (ConstraintViolation<Country> violation : violations) {
-			errors.add(violation.getMessage());
-		}
-
-		if (violations.size() > 0) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errors.toString());
-		}
-
 		return country;
 	}
 
