e3c32fa4281963ca589af61e489ca13484361686
AsMetOP <176515010+AsMetOP@users.noreply.github.com>
2026-07-15 06:48:57 +0530

    Week3: Ex25 - DecodeUsernameFromHeader complete

--- Week3/SpringREST/JWT_handson/spring-learn/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
+++ Week3/SpringREST/JWT_handson/spring-learn/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
@@ -1,5 +1,6 @@
 package com.cognizant.springlearn.controller;
 
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,18 +13,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticationController {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationController.class);
 
-	@GetMapping("/authenticate")
-	public Map<String, String> authenticate(@RequestHeader("Authorization") String authHeader) {
-		LOGGER.info("Start");
-		LOGGER.debug(authHeader);
+    @GetMapping("/authenticate")
+    public Map<String, String> authenticate(@RequestHeader("Authorization") String authHeader) {
+        LOGGER.info("Start");
+        LOGGER.debug(authHeader);
 
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("token", "");
+        String user = getUser(authHeader);
 
-		LOGGER.info("End");
-		return map;
-	}
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("token", "");
+
+        LOGGER.info("End");
+        return map;
+    }
+
+    private String getUser(String authHeader) {
+        LOGGER.debug("Start getUser");
+
+        String encodedCredentials = authHeader.replace("Basic ", "");
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedCredentials);
+        String decodedString = new String(decodedBytes);
+        String user = decodedString.substring(0, decodedString.indexOf(":"));
+
+        LOGGER.debug("user: {}", user);
+        LOGGER.debug("End getUser");
+        return user;
+    }
 
 }
