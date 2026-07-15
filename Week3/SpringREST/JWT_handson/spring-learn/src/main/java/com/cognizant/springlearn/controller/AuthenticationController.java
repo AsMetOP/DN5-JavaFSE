1e8120a222f6602d37357a607ffa37cdeb08b74c
AsMetOP <176515010+AsMetOP@users.noreply.github.com>
2026-07-15 06:53:41 +0530

    Week3: Ex26 - GenerateJwt complete

--- Week3/SpringREST/JWT_handson/spring-learn/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
+++ Week3/SpringREST/JWT_handson/spring-learn/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.cognizant.springlearn.controller;
 
 import java.util.Base64;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,36 +11,56 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 @RestController
 public class AuthenticationController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationController.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationController.class);
 
-    @GetMapping("/authenticate")
-    public Map<String, String> authenticate(@RequestHeader("Authorization") String authHeader) {
-        LOGGER.info("Start");
-        LOGGER.debug(authHeader);
+	@GetMapping("/authenticate")
+	public Map<String, String> authenticate(@RequestHeader("Authorization") String authHeader) {
+		LOGGER.info("Start");
+		LOGGER.debug(authHeader);
 
-        String user = getUser(authHeader);
+		String user = getUser(authHeader);
+		String token = generateJwt(user);
 
-        Map<String, String> map = new HashMap<String, String>();
-        map.put("token", "");
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("token", token);
 
-        LOGGER.info("End");
-        return map;
-    }
+		LOGGER.info("End");
+		return map;
+	}
 
-    private String getUser(String authHeader) {
-        LOGGER.debug("Start getUser");
+	private String getUser(String authHeader) {
+		LOGGER.debug("Start getUser");
 
-        String encodedCredentials = authHeader.replace("Basic ", "");
-        byte[] decodedBytes = Base64.getDecoder().decode(encodedCredentials);
-        String decodedString = new String(decodedBytes);
-        String user = decodedString.substring(0, decodedString.indexOf(":"));
+		String encodedCredentials = authHeader.replace("Basic ", "");
+		byte[] decodedBytes = Base64.getDecoder().decode(encodedCredentials);
+		String decodedString = new String(decodedBytes);
+		String user = decodedString.substring(0, decodedString.indexOf(":"));
 
-        LOGGER.debug("user: {}", user);
-        LOGGER.debug("End getUser");
-        return user;
-    }
+		LOGGER.debug("user: {}", user);
+		LOGGER.debug("End getUser");
+		return user;
+	}
+
+	private String generateJwt(String user) {
+		LOGGER.debug("Start generateJwt");
+
+		JwtBuilder builder = Jwts.builder();
+		builder.setSubject(user);
+		builder.setIssuedAt(new Date());
+		builder.setExpiration(new Date((new Date()).getTime() + 1200000));
+		builder.signWith(SignatureAlgorithm.HS256, "secretkey");
+		String token = builder.compact();
+
+		LOGGER.debug("token: {}", token);
+		LOGGER.debug("End generateJwt");
+		return token;
+	}
 
 }
