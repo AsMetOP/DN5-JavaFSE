dbf96892dfa93784e12dc22d90fbb2cc469f7090
AsMetOP <176515010+AsMetOP@users.noreply.github.com>
2026-07-22 01:53:20 +0530

    Week4: Ex1a UserService - REST CRUD with MySQL complete

---
+++ Week4/Microservices/0_Sample_Microservices_exercises/user-service/src/main/java/com/cognizant/userservice/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.cognizant.userservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.userservice.entity.User;
+import com.cognizant.userservice.repository.UserRepository;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
