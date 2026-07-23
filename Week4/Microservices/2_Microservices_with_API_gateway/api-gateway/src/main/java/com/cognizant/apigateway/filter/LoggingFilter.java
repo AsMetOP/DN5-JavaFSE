3465c2d55fe0476ac7fa8d86301da88ca5793c7b
AsMetOP <176515010+AsMetOP@users.noreply.github.com>
2026-07-24 02:00:02 +0530

    Completed Week4 Exercise 2 - API Gateway

---
+++ Week4/Microservices/2_Microservices_with_API_gateway/api-gateway/src/main/java/com/cognizant/apigateway/filter/LoggingFilter.java
@@ -0,0 +1,18 @@
+package com.cognizant.apigateway.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.HandlerFilterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class LoggingFilter {
+
+    @Bean
+    public HandlerFilterFunction<ServerResponse, ServerResponse> logFilter() {
+        return (request, next) -> {
+            System.out.println("====> Request URL : " + request.uri());
+            return next.handle(request);
+        };
+    }
+}
