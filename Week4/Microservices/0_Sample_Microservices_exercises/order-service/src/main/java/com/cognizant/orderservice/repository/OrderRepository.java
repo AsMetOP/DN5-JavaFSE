18569fe1f3dfe0667b48b4da62c1f863853e7a8f
AsMetOP <176515010+AsMetOP@users.noreply.github.com>
2026-07-24 09:12:18 +0530

    Untrack Week4 Microservices 0_ reference folders, keep locally via gitignore

--- Week4/Microservices/0_Sample_Microservices_exercises/order-service/src/main/java/com/cognizant/orderservice/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.orderservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognizant.orderservice.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- Week4/Microservices/0_Sample_Microservices_exercises/user-service/src/main/java/com/cognizant/userservice/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.userservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognizant.userservice.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
